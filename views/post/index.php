64d908626066d7814cb3682f5b59dba23e935dd5
qqqwwwbbb <usecaut@gmail.com>
2023-06-25 15:50:03 +0700

    category

--- views/post/index.php
+++ views/post/index.php
@@ -7,7 +7,12 @@ $this->title = Yii::$app->name;
         <div class="col-lg-4 mb-3">
             <?php foreach($posts as $post): ?>
             <div class="col-md-3">
-                <div class="single-post">
+                <a class="single-post">
+                    <h3><span> Category:
+                            <a href="<?= \yii\helpers\Url::to(['category/view', 'alias' => $post->category->alias]) ?>">
+                            <?= $post->category->title ?>
+                </a>
+                            </span></h3>
                     <h3><a href="<?= \yii\helpers\Url::to(['post/view', 'id' => $post->id]) ?>"><?= $post->title ?></a> </h3>
                     <p>
                         <?= $post->excerpt ?>
